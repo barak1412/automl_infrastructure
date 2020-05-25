a2eee4a839de8368894e8b808f452ffb0730378f
barak1412 <barak1413@gmail.com>
2020-05-25 17:55:13 +0300

    Added GraphDomainMapper.

---
+++ automl_infrastructure/interpretation/lime/lime_graph.py
@@ -0,0 +1,32 @@
+from lime.lime_base import LimeBase
+from lime.explanation import Explanation, DomainMapper
+from functools import partial
+from sklearn.utils import check_random_state
+import scipy as sp
+import sklearn
+
+
+class GraphDomainMapper(DomainMapper):
+    """Maps nodes indices to vertices names"""
+
+    def __init__(self, neighbors_lst):
+        """Initializer.
+        Args:
+            neighbors_lst: list of neighbors as strings.
+        """
+        super().__init__()
+
+        # sort neighbors in lexicographical order
+        self._neighbors_lst = sorted(neighbors_lst)
+
+    def map_exp_ids(self, exp):
+        """Maps indices to nodes names.
+        Args:
+            exp: list of tuples [(idx, weight), (idx,weight)]
+        Returns:
+            list of tuples (node, weight)
+        """
+
+        exp = [(self._neighbors_lst[x[0]], x[1]) for x in exp]
+        return exp
+
