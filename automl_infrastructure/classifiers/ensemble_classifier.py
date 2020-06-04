c252c93c85ac82e20d2652a4b1dd5c4017e5c0a8
barak1412 <barak1413@gmail.com>
2020-06-04 12:23:55 +0300

    Fixed ensemble classifier to support vector feature type.

--- automl_infrastructure/classifiers/ensemble_classifier.py
+++ automl_infrastructure/classifiers/ensemble_classifier.py
@@ -1,5 +1,7 @@
 from automl_infrastructure.classifiers import Classifier
 import numpy as np
+import pandas as pd
+from automl_infrastructure.utils import random_str
 
 
 class EnsembleClassifier(Classifier):
@@ -10,13 +12,29 @@ class EnsembleClassifier(Classifier):
         self._ensemble_model = ensemble_model
         self._ensemble_features = ensemble_extra_features
 
-    def _prepare_features(self, x):
+        # used to create dataframe over ensemble features
+        self._temporary_ensemble_features = None
+
+    def _prepare_features(self, x, recreate_features_names=False):
         new_features = []
         for input_model in self._input_models:
             new_features.append(input_model.predict_proba(x))
         if self._ensemble_features is not None:
             new_features.append(x[self._ensemble_features])
-        return np.concatenate(new_features, axis=1)
+
+        features_array = np.concatenate(new_features, axis=1)
+        # create random features names if none supplied
+        if recreate_features_names:
+            new_features_names = ['FEATURE_{}_{}'.format(i, random_str(length=8))
+                                  for i in range(features_array.shape[1])]
+            self._temporary_ensemble_features = new_features_names
+        else:
+            if self._temporary_ensemble_features is None:
+                raise Exception('You must initialize new features names.')
+            new_features_names = self._temporary_ensemble_features
+
+        new_features_df = pd.DataFrame(features_array, columns=new_features_names)
+        return new_features_df
 
     def fit(self, x, y, **kwargs):
         # train input models
@@ -24,7 +42,7 @@ class EnsembleClassifier(Classifier):
             input_model.fit(x, y, **kwargs)
 
         # train ensemble model based on input models predictions
-        final_x = self._prepare_features(x)
+        final_x = self._prepare_features(x, recreate_features_names=True)
         self._ensemble_model.fit(final_x, y, **kwargs)
 
     def predict(self, x):
