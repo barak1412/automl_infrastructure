9c520d451d42d4b31e13bfc62320a3126695fe4d
barak1412 <barak1413@gmail.com>
2020-06-11 11:31:41 +0300

    Added comments to metrics utils.

--- automl_infrastructure/experiment/metrics/utils.py
+++ automl_infrastructure/experiment/metrics/utils.py
@@ -2,6 +2,15 @@ from automl_infrastructure.experiment.metrics import MetricFactory, Metric, Obje
 
 
 def parse_metric(metric):
+    """
+    Receive metric and return callable that calculate the metric.
+
+    :param metric: the metric to parse.
+    :type metric: str, callable or :class:`automl_infrastructure.experiment.metrics.base.Metric`
+
+    :return: callable (list of Any, list of :class:`automl_infrastructure.classifiers.base.ClassifierPrediction` ->
+     number or list of numbers) that calculate the metric.
+    """
     if isinstance(metric, str):
         metric_obj = MetricFactory.create(metric)
         metric_func = lambda y_true, classifier_prediction: metric_obj.measure(y_true, classifier_prediction)
@@ -15,6 +24,15 @@ def parse_metric(metric):
 
 
 def parse_objective(objective):
+    """
+    Receive objective (aggregated metric) and return callable that calculate the metric.
+
+    :param objective: the objective to parse.
+    :type objective: str or callable
+
+    :return: callable (list of Any, list of :class:`automl_infrastructure.classifiers.base.ClassifierPrediction` ->
+     number) that calculate the metric.
+    """
     if callable(objective):
         return objective
     elif isinstance(objective, str):
