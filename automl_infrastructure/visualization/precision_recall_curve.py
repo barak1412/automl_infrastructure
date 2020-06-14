a0f78e491dceddd61afeee4a35988a2faf304777
barak1412 <barak1413@gmail.com>
2020-06-14 17:36:03 +0300

    Added comments to precision_recall_curve.

--- automl_infrastructure/visualization/precision_recall_curve.py
+++ automl_infrastructure/visualization/precision_recall_curve.py
@@ -8,8 +8,18 @@ import numpy as np
 
 
 class PrecisionRecallCurve(Visualization):
+    """
+    Precision-recall curve implementation follows the :class:`automl_infrastructure.visualization.base.Visualization` interface.
+    """
 
     def __init__(self, max_classes_per_figure=5, n_thresholds=None):
+        """
+        :param max_classes_per_figure: maximum number of classes inside each graph.
+        :type max_classes_per_figure: int ,optional
+
+        :param n_thresholds: custom thresholds (ticks) for the precision-recall curve.
+        :type n_thresholds: list ,optional
+        """
         self._classes_curves = {}
         self._max_classes_per_figure = max_classes_per_figure
         self._n_thresholds = n_thresholds
@@ -17,6 +27,26 @@ class PrecisionRecallCurve(Visualization):
     @staticmethod
     def _custom_precision_recall_curve(y_true, probas_pred, threshold_start=0.0, threshold_finish=0.999,
                                   threshold_amount=50):
+        """
+        Creates precision-recall curve with custom thresholds (ticks).
+
+        :param y_true: true labels.
+        :type y_true: pandas.Series or list
+
+        :param probas_pred: list of classes probabilities list
+        :type probas_pred: list of list
+
+        :param threshold_start: starting tick.
+        :type threshold_start: number ,optional
+
+        :param threshold_finish: ending tick.
+        :type threshold_finish: number ,optional
+
+        :param threshold_amount: number of ticks.
+        :type threshold_amount: int ,optional
+
+        :return: precision-recall curve with custom thresholds
+        """
         x = np.linspace(threshold_start, threshold_finish, num=threshold_amount)
         precisions = []
         recalls = []
@@ -36,9 +66,8 @@ class PrecisionRecallCurve(Visualization):
         binarizer.fit(classes)
         y_true_combined_binary = binarizer.transform(y_true_combined)
 
-        
-        if(len(classes)==2):
-            y_true_combined_binary = np.column_stack([(1- y_true_combined_binary)[:,0],y_true_combined_binary[:,0]])
+        if len(classes) == 2:
+            y_true_combined_binary = np.column_stack([(1 - y_true_combined_binary)[:, 0], y_true_combined_binary[:, 0]])
         # generate metric for every class
         for idx, c in enumerate(classes):
             if self._n_thresholds is None:
@@ -50,6 +79,12 @@ class PrecisionRecallCurve(Visualization):
             self._classes_curves[c] = (precision_lst, recall_lst, ticks)
 
     def get_curve(self, class_name):
+        """
+        :param class_name: the name of the class.
+        :type class_name: str
+
+        :return: precision-recall curve of the given class.
+        """
         return self._classes_curves[class_name]
 
     @property
